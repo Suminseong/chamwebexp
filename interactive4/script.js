bd7a1363398bf83ec5e1475406551c5d5bb30ca0
Suminseong <epwyqqu309@gmail.com>
2024-05-09 03:03:25 +0900

    image added. loading page added.

--- interactive4/script.js
+++ interactive4/script.js
@@ -4,7 +4,7 @@ const divSection = document.getElementsByClassName('wrap-section');
 let page = 0;
 const endPage = divSection.length - 1;
 
-function scrolly(i) {
+function scrolly(i) { //스크롤시 Div가 하나씩 넘어가는 효과
     i.preventDefault();
     if (i.deltaY > 0) {
         page++;
@@ -21,9 +21,13 @@ function scrolly(i) {
     divMain.style.top = page * -100 + 'vh';
 }
 
-//쓰로틀링이라는 좋은 기술을 사용합니다. 연속 이벤트의 첫번째만 받으면 일정 시간동안 입력을 처리 하지 않습니다.
-//여기서는 Timer가 없는지 검사하고, 존재한다면 이벤트를 취소한다. 존재하지 않는다면 새로운 timer를 생성하고, 이벤트는 실행된다.
+//로딩탭입니다. 이미지, 페이지 등 뭔가 로딩중이라면 띄웁니다.
+window.addEventListener('load', function() {
+    document.getElementById('loading-screen').style.display = 'none';
+});
 
+//쓰로틀링, 연속 이벤트의 첫번째만 받으면 일정 시간동안 입력을 처리 하지 않습니다.
+//여기서는 Timer가 없는지 검사하고, 존재한다면 이벤트를 취소하빈다. 존재하지 않는다면 새로운 timer를 생성하고, 이벤트는 실행됩니다.
 let timer;
 window.addEventListener('wheel', (i) => {
     if (!timer) {
