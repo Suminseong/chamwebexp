d2675e9d4ad857517c1363bbf736bef5d177b1e6
Suminseong <76616743+Suminseong@users.noreply.github.com>
2024-05-21 13:51:09 +0900

    new img added, intro complete

--- interactive5/script.js
+++ interactive5/script.js
@@ -1,18 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const box = document.getElementById('box');
-    const trigger = document.getElementById('scroll-trigger');
+const headTag = document.querySelector('header');
+const introTag = document.querySelector('intro-container');
+const introSec = document.querySelector('section0');
 
-    window.addEventListener('scroll', function() {
-        const scrollPosition = window.scrollY + window.innerHeight;
-        const triggerPosition = trigger.getBoundingClientRect().top + window.scrollY;
+document.addEventListener('scroll', function () {
 
-        // 특정 위치에 도달했을 때 배경 색상 변경
-        if (scrollPosition > triggerPosition) {
-            document.body.style.backgroundColor = 'lightblue';
-            box.classList.add('visible');
-        } else {
-            document.body.style.backgroundColor = '';
-            box.classList.remove('visible');
-        }
-    });
 });
